74420bac781f9f878d719c2e960663e0ed39019d
Yoshiben <48318488+Yoshiben@users.noreply.github.com>
2025-04-25 14:20:38 +0100

    Update players.js to add Jake and Zak

--- players.js
+++ players.js
@@ -1,5 +1,25 @@
 const defaultPlayers = [
-  "Bailey Fulton", "Ben Attle", "Bradley Travis", "Chad Ogden", "Craig Ingham", "Danny Minshull", "Dom Johnson", "James Shooter", 
-  "Joe Duffy", "Joe Kinnear", "Joe Rawlins", "John Sheard (Shed)", "Jorge Westin-Pearson", "Kyle Carrington", "Leon Khan", 
-  "Niall Raymond", "Ross Barney", "Sohrob Rostampour", "Tom Kelton", "Tom Wright", "Zach Murphy"
+  "Bailey Fulton", 
+  "Ben Attle", 
+  "Bradley Travis", 
+  "Chad Ogden", 
+  "Craig Ingham", 
+  "Danny Minshull", 
+  "Dom Johnson", 
+  "Jake Halstead",
+  "James Shooter", 
+  "Joe Duffy", 
+  "Joe Kinnear", 
+  "Joe Rawlins", 
+  "John Sheard (Shed)", 
+  "Jorge Westin-Pearson", 
+  "Kyle Carrington", 
+  "Leon Khan", 
+  "Niall Raymond", 
+  "Ross Barney", 
+  "Sohrob Rostampour", 
+  "Tom Kelton", 
+  "Tom Wright", 
+  "Zach Murphy",
+  "Zak Waddington"
 ];
